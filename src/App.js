f1741bcd9702e08f5a348612729d79b0f3289a2c
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-15 20:11:08 +0300

    create html elements to append data

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import LangsSelect from './Components/LangsSelect';
 
 class App extends Component {
   state={
-    edition: 'Arabic',
+    edition: 'ara-kingfahadquranc-la',
   }
   handleLanguage = (newValue) => {
     return this.setState({edition: newValue.value})
   };
   render() {
     return (<div className="App">
-        Hello
-        <fieldset>
+      <header>
+        <h1>
+          Holy Quran | القرآن الكريم 
+        </h1>
+      </header>
+      <div className='hint'>
+        <p>Browse editions of the Holy Quran in all languages all over the world</p>
+      </div>
+        <fieldset className='filter'>
             <legend>
                 Filter
             </legend>
             <LangsSelect onChange={this.handleLanguage} />
         </fieldset>
+        <section className='data-section'>
+          <div className='data-container'>
+            <ul>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+              <li> الحمد لله رب العالمين </li>
+            </ul>
+          </div>
+        </section>
       </div>
     );
   }
