3855bba9fcc93455b6cad4c3e01ec722cbd52c11
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-09-15 21:07:17 +0300

    add selectedchapter to the states, create handleChange func, and render the chapters list #10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import LangsSelect from './Components/LangsSelect';
+import ChaptersList from './Components/ChaptersList';
 
 class App extends Component {
   state={
     edition: 'Arabic',
+    selectedChapter: 1,
   }
   handleLanguage = (newValue) => {
     return this.setState({edition: newValue.value})
+  };
+  handleChange = (e, key) => {
+    this.setState({[key]: e.value})
   };
   render() {
     return (<div className="App">
@@ -17,6 +22,7 @@ class App extends Component {
                 Filter
             </legend>
             <LangsSelect onChange={this.handleLanguage} />
+            <ChaptersList handleChange={this.handleChange}/>
         </fieldset>
       </div>
     );
