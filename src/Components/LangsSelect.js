8ae2d3e9bedc368b45fefa7e852502da544c3f61
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-15 19:20:24 +0300

    create the component to fetch edditions Relates #4

--- src/Components/LangsSelect.js
+++ src/Components/LangsSelect.js
@@ -1,17 +1,22 @@
 import Creatable from 'react-select/creatable';
-import languages from '../data';
+import fetchEditions from './fetchEditions';
 const LangsSelect = (props) => {
-    const options = []
-    languages.map((ele) => {
-        const option = {
-            value: ele,
-            label: ele
-        }
-        return options.push(option)
-        
+    const options = [] // options data will be pushed here
+    fetchEditions()
+    .then(data => {
+        return data.map(ele => {
+            const option = { // set the value and label for the option
+                value: ele.name,
+                label: `${ele.language} - ${ele.author}`
+            }
+            options.push(option);
+        })
     })
-return (
-    <Creatable options={options} onChange={props.onChange} />
+    .catch(err => {
+        console.log(err.message);
+    })
+return (// create select element and its options
+    <Creatable options={options} onChange={props.onChange} /> 
 
 )
 }
