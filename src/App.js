c2164062240dea815a286a58b9b63c4d87121ab1
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-15 15:27:59 +0300

    add fieldset of languages  to app.js Relates #4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { Component } from 'react';
 import './App.css';
+import LangsSelect from './Components/LangsSelect';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends Component {
+  state={
+    langValue: 'Arabic',
+
+  }
+  handleChange = (e) => {
+    console.log(e.target);
+  }
+  render() {
+    return (<div className="App">
+        Hello
+        <fieldset>
+            <legend>
+                Filter
+            </legend>
+            <LangsSelect onChange={this.handleChange} />
+            <LangsSelect onChange={this.handleChange} />
+
+        </fieldset>
+      </div>
+    );
+  }
 }
 
 export default App;
