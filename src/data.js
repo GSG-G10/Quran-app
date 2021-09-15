15a7235ec3e3408d8f9926e5fa422d53f07b1425
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-15 15:25:20 +0300

    store languages selectors Relates #4

---
+++ src/data.js
@@ -0,0 +1,5 @@
+const languages = [ 'Afar', 'Achinese', 'Afrikaans', 'Amharic', 'Arabic', 'Assamese', 'Azerbaijani', 'Bambara', 'Bengali', 'Berber', 'Bosnian', 'Bulgarian', 'Catalan', 'Czech', 'Dagbani', 'Danish', 'German', 'Divehi', 'English', 'Esperanto', 'Persian', 'Filipino', 'Finnish', 'French', 'Gujarati', 'Hausa', 'Hebrew', 'Hindi', 'Croatian', 'Hungarian', 'Iranun', 'Indonesian', 'Italian', 'Javanese', 'Japanese', 'Kannada', 'Kazakh', 'Khmer', 'Kinyarwanda', 'Kirghiz', 'Kurmanji', 'Kendayan', 'Korean', 'Kurdish', 'Latin', 'Ganda', 'Luyia', 'Malayalam', 'Marathi', 'Macedonian', 'Maltese', 'Maranao', 'Malay', 'Burmese', 'Nepali', 'Dutch', 'Norwegian', 'Chichewa', 'Oromo', 'Panjabi', 'Polish', 'Portuguese', 'Pushto', 'Romanian', 'Rundi', 'Russian', 'Sinhala', 'Slovak', 'Shona', 'Sindhi', 'Somali', 'Sotho', 'Spanish', 'Albanian', 'Swahili', 'Swedish', 'Tamil', 'Tatar', 'Telugu', 'Tajik', 'Thai', 'Turkish', 'Uighur', 'Ukrainian', 'Urdu', 'Uzbek', 'Vietnamese', 'Xhosa', 'Yoruba', 'Chinese(traditional)', 'Chinese(simplified)', 'Zulu' ]
+
+export default languages;
+
+
